1a03dda0e819c4608dba72b372759536dd9cb054
Gurman Singh <77499829+gurman007@users.noreply.github.com>
2022-07-26 00:30:45 +0530

    Time: 19 ms (22.01%), Space: 9.5 MB (86.60%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,52 +1,32 @@
-#define vi vector<int>
-#define ll long long
-class Solution {
-public:
-    
-    int singleNumber(vector<int>& nums) 
+class Solution
 {
-    vi sum(32, 0);
-    ll st_bit = 1;
-    for(int i=0, k=0; i < 32; ++i, ++k)
-    {
-        ll mask = (st_bit << i);
-        for(int j = 0; j < nums.size(); ++j)
+public:
+int singleNumber(vector<int>& nums) {
+        
+        long long int ans = 0;
+        vector<int>cnt(32,0);   // count bit array => Constant Extra Space
+        for(int i=0;i<nums.size();i++)
         {
-            if((mask & nums[j]) > 0)
-                sum[k] += 1;
-        }
-        sum[k] %= 3;
-    }
-    
-    // Evaluating Unique Number..
-    ll unique_no = 0, flag = 1; 
-    signed int unique_no2 = 0;
-    if(sum[31] == 0)          // i.e number is positive as MSB = 0;
-    {
-        for(int i = 0; i < 32; ++i)
-        {
-            if(sum[i] == st_bit)
+            int j=31;
+            long long int n = abs(nums[i]);  // int-Range	=>	[ -2,147,483,648 to 2,147,483,647 ]
+            while(n)                    // abs(-2,147,483,648) = 2,147,483,648 not stored in 'int'
             {
-                ll pow = (st_bit << i);
-                unique_no += pow;
+                cnt[j] += n&1;
+                n >>= 1; 
+                j--;
             }
         }
-    }
-    else
-    {
-        flag = 0;
-        string s;
-        for(int i = sum.size()-1; i >= 0; --i)
+        
+        for(int j=31;j>=0;j--)
         {
-            char c = sum[i] + '0';
-            s += c;
+            int bit = cnt[j]%3;
+            if(bit)
+             ans += 1 <<(31-j); // 1 * 2^(31-j)
         }
-        auto ul = stoul(s,nullptr,2);
-        unique_no2 = ul;
-    } 
-    if(flag) 
-        return unique_no;
-    else
-        return unique_no2;
+        
+        int c=0;
+        for(auto x:nums)   //for negative numbers
+          if(x==ans) {c++;}   //if our calculated value 'ans' is present in array, then increment c
+        return c==1?ans:-ans; 
     }
 };
